44c94ce1d8a4e1f614f083eacabe3715cf85bd55
Yuliia Skabitskaya <yulik777@i.ua>
2022-05-04 14:13:48 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
-import styles from './App.css';
+import styles from './App.css'
 
 import Card from './components/cards.jsx';
 
 const cards = [
   {
     name:"Безлимитный 300",
-    price:"300",
-    speed:"10мБит/c"
+    price:"300/мес",
+    speed:"до 10 мБит/cек",
+    text:"Объем включенного трафика не ограничен"
   },
   {
     name:"Безлимитный 450",
-    price:"450",
-    speed:"10мБит/c"
+    price:"450/мес",
+    speed:"до 50 мБит/cек",
+    text:"Объем включенного трафика не ограничен"
   },
   {
     name:"Безлимитный 550",
-    price:"550",
-    speed:"10мБит/c",
+    price:"550/мес",
+    speed:"до 100 мБит/cек",
+    text:"Объем включенного трафика не ограничен",
     isSelected: true
   },
   {
     name:"Безлимитный 1000",
-    price:"1000",
-    speed:"10мБит/c"
+    price:"1000/мес",
+    speed:"до 200 мБит/cек",
+    text:"Объем включенного трафика не ограничен"
   }
 ]
 
@@ -32,17 +36,14 @@ function App (){
   
   return (
     <div>
-    <div className={this.props.theme.App}>
+    <div className={App}>
     {
       cards.map((card) =>
-      <Card name={card.name} price={card.price} speed={card.speed} 
+      <Card name={card.name} price={card.price} speed={card.speed} text={card.text}
       isSelected={card.isSelected}></Card>
       )
   }
 </div>
-    <div className="App-button-container">
-      <button >Try now</button>
-    </div>
     </div>
   );
 }
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -3,15 +3,15 @@ import'./cards.scss';
 
 function Card (props) { 
   
-   const {name, price, speed, isSelected} = this.props;
+   const {name, price, speed, text, isSelected} = props;
    return (
     <div className="App"> 
+    
        <div className={"card " + (isSelected? "selected":" ")}>
          <div className="card-name">{props.name}</div>
-         <div className="card-text"> Объем трафика не ограчинен</div>
-         <div className="card-speed"> {props.speed}</div>
          <div className="card-price"> {props.price}</div>
-         <div> <button className='button'>Buy</button></div>
+         <div className="card-speed"> {props.speed}</div>
+         <div className="card-text"> {props.text}</div>
          </div>
          </div>
    );
