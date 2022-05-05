4200cb31dd1512841d2d3619a5490024aeb0f3e4
Yuliia Skabitskaya <yulik777@i.ua>
2022-05-05 15:18:13 +0300

    color

--- src/App.js
+++ src/App.js
@@ -7,26 +7,26 @@ import Card from './components/cards.jsx';
 const cards = [
   {
     name:"Безлимитный 300",
-    price:"300/мес",
+    price:"300",
     speed:"до 10 мБит/cек",
     text:"Объем включенного трафика не ограничен"
   },
   {
     name:"Безлимитный 450",
-    price:"450/мес",
+    price:"450",
     speed:"до 50 мБит/cек",
     text:"Объем включенного трафика не ограничен"
   },
   {
     name:"Безлимитный 550",
-    price:"550/мес",
+    price:"550",
     speed:"до 100 мБит/cек",
     text:"Объем включенного трафика не ограничен",
     isSelected: true
   },
   {
     name:"Безлимитный 1000",
-    price:"1000/мес",
+    price:"1000",
     speed:"до 200 мБит/cек",
     text:"Объем включенного трафика не ограничен"
   }
@@ -37,14 +37,17 @@ function App (){
   return (
     <div>
     <div className={App}>
+  <div className='container'>
     {
       cards.map((card) =>
       <Card name={card.name} price={card.price} speed={card.speed} text={card.text}
       isSelected={card.isSelected}></Card>
       )
   }
+  </div>
+  </div>
 </div>
-    </div>
+  
   );
 }
 
--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -6,14 +6,17 @@ function Card (props) {
    const {name, price, speed, text, isSelected} = props;
    return (
     <div className="App"> 
-    
+   
        <div className={"card " + (isSelected? "selected":" ")}>
          <div className="card-name">{props.name}</div>
-         <div className="card-price"> {props.price}</div>
+         <div className='card-totalprice'>
+             <p className="currency">usd</p>
+             <span className="card-price">{props.price}</span>  /мес </div>
          <div className="card-speed"> {props.speed}</div>
          <div className="card-text"> {props.text}</div>
          </div>
          </div>
+         
    );
   }
   export default Card;
